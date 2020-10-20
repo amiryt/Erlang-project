64bff4f30b5671d464546538f4032b27aedd4d18
amiryt <amiryt@post.bgu.ac.il>
2020-10-20 11:40:41 +0300

    ReadMe new

---
+++ src/computers.hrl
@@ -0,0 +1,16 @@
+%%%-------------------------------------------------------------------
+%%% @author amiryt
+%%% @copyright (C) 2020, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 20. אוק׳ 2020 11:05
+%%%-------------------------------------------------------------------
+-author("amiryt").
+
+-define(PC_SERVER, 'serverNode@132.72.18.25').
+-define(PC_MONITOR, 'monitorNode@132.72.18.27').
+-define(PC_RESMONITOR, 'resmonitorNode@132.72.18.26').
+-define(PC_INPUTLAYER, 'snnNode@132.72.18.18').
+-define(PC_OUTPUTLAYER, 'outputlayerNode@132.72.18.26').
+-define(PC_GRAPHICS, 'graphicsNode@132.72.18.25').
